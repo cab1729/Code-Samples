aa749a04651abe9813d2833e32f2584d13023b7b
Jose Menes <jmenes1729@gmail.com>
2012-07-13 17:06:00 -0400

    added code to retrieve employee email from db. JPA/Hibernate support.

--- osgiservice/src/org/cab1729/osgi/service/GetEmpInfo.java
+++ osgiservice/src/org/cab1729/osgi/service/GetEmpInfo.java
@@ -1,11 +1,26 @@
 package org.cab1729.osgi.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.cab1729.osgi.entity.Employee;
+
 public class GetEmpInfo implements IGetEmpInfo {
+	
+	@PersistenceContext(unitName="osgiservice")
+	EntityManager em;
 
 	@Override
 	public String getEmpEmail(long id) {
-		// TODO add code to get from db
-		return "jmenes1729@gmail.com";
+		String email;
+		try {
+			Employee employee = em.find(Employee.class, Long.valueOf(id)); 
+			email = employee.getEmail();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			email = "NOT FOUND/ERROR";
+		} 
+		return email;
 	}
 
 }
